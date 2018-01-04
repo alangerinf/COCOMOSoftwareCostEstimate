0e524006cf878dbfdd7c0e0116669cf90d6868af
informatico40 <informatico240@gmail.com>
2018-01-03 20:37:17 -0500

    COCOMO: sending to the repo

---
+++ src/proyectotopicossw/ProyectoTopicosSW.java
@@ -0,0 +1,16 @@
+package proyectotopicossw;
+
+import Interface.Welcome;
+
+/**
+ *
+ * @author Informatico40
+ */
+public class ProyectoTopicosSW {
+
+    public static void main(String[] args) {
+        Welcome welcome = new Welcome();
+        welcome.setVisible(true);
+    }
+    
+}
